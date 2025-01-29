e8b588d9d59a415e7e907975b2197f1fb10e5a75
dennis-mmachoene <dennismmachoene@gmail.com>
2025-01-29 10:44:45 +0200

    Implement the complete layout

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -11,7 +11,7 @@ const BankCard = ({
   return (
     <div className="flex flex-col">
       <Link href="/" className="bank-card">
-        <div className="bank-card-content">
+        <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">
               {account.name || userName}
@@ -27,17 +27,17 @@ const BankCard = ({
                 {userName}
                 </h1>
                 <h2 className="text-12 font-semibold text-white">
-                ** / ** 
+                ●● / ●● 
                 </h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white"> 
-              **** **** **** <span className='text-16'>R{account.mask}</span>
+            ●●●● ●●●● ●●●● <span className='text-16'>{1020}</span>
             </p>
           </article>
         </div>
         <div className="bank-card_icon">
           <Image src='/icons/Paypass.svg' width={20} height={24} alt='pay' />
-          <Image src='/icons/mastercard.svg' width={45} height={32} alt="mastercard"/>
+          <Image src='/icons/mastercard.svg' width={45} height={32} alt="mastercard" className="ml-5"/>
         </div>
 
         <Image src='/icons/lines.png' width={316} height={190} alt='lines' className="absolute top-0 left-0" />
